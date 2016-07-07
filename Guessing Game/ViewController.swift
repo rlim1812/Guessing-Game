996789a27700e9470f7612cf8031412add0b2e6d
Ryan Lim <rlim1812@gmail.com>
2016-07-06 20:34:28 -0700

    "Added designs"

--- Guessing Game/ViewController.swift
+++ Guessing Game/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let numberOfHands = Int(arc4random_uniform(5)) + 1
+    
+    
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var Response: UILabel!
+    
+    @IBAction func ActionButton(sender: AnyObject) {
+        
+        let numText = Int(TextField.text!)!
+        
+        if numText == numberOfHands {
+            Response.text = "Hurray! You chose the right number!"
+        } else {
+            Response.text = "Sorry, that is the wrong number. Try again."
+        }
+        
 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
